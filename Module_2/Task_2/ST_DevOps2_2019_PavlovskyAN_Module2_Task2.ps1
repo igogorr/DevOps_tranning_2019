d068587a30bc737e20a7db4ac1940adbcfcd4b99
pavlovskyantony <pavlovsky.antony@gmail.com>
2019-06-05 18:00:32 +0300

    Create ST_DevOps2_2019_PavlovskyAN_Module2_Task2.ps1
    Module 2 task 2 has been completed

---
+++ Module_2/Task_2/ST_DevOps2_2019_PavlovskyAN_Module2_Task2.ps1
@@ -0,0 +1,47 @@
+<#
+EPAM Systems, RDDep.
+
+
+MTN.Win.02
+Автоматизация администрирования с PowerShell 2.0
+Конвейеризация, вывод. Использование поставщиков и дисков
+
+
+
+
+
+ #>
+#Задание 2
+
+# 1.	Просмотреть содержимое ветви реeстра HKCU
+Get-ChildItem HKCU:
+# 2.	Создать, переименовать, удалить каталог на локальном диске
+New-Item -Name Folder -Type Directory -Path C:\Temp\\
+Rename-Item -Path C:\Temp\Folder -NewName Folder1
+Remove-Item -Path C:\Temp\Folder
+# 3.	Создать папку C:\M2T2_ФАМИЛИЯ. Создать диск ассоциированный с папкой C:\M2T2_ФАМИЛИЯ.
+New-Item -Name Folder -Type Directory -Path ‘C:\M2T2_PavlovskyAN\’
+New-PSDrive -Name "MyTestDrive" -Root ‘C:\M2T2_PavlovskyAN\’ -PSProvider "FileSystem"
+# 4.	Сохранить в текстовый файл на созданном диске список запущенных(!) служб. Просмотреть содержимое диска. Вывести содержимое файла в консоль PS.
+Get-Service | Out-File 'MyTestDrive:\1.txt'
+Get-ChildItem MyTestDrive:
+Get-Content MyTestDrive:\1.txt
+# 5.	Просуммировать все числовые значения переменных текущего сеанса.
+$res=Get-Variable | Where-Object -FilterScript {$_.Value.GetType() -like 'int*'}
+$res | foreach {$summ += $_.value}
+$summ
+# 6.	Вывести список из 6 процессов занимающих дольше всего процессор.
+Get-Process | Where-Object -FilterScript {$PSItem.CPU -ge 70} | Sort-Object CPU -Descending
+# 7.	Вывести список названий и занятую виртуальную память (в Mb) каждого процесса, разделённые знаком тире, при этом если процесс занимает более 100Mb – выводить информацию красным цветом, иначе зелёным.
+Get-Process | Select-Object -Property name,@{n="Memory"; e={$_.VirtualMemorySize/1Mb}} | ForEach-Object {if ($_.VirtualMemorySize -ge 100.0) {Write-Host -ForegroundColor red $_.Name,$_.VirtualMemorySize} else {Write-Host -ForegroundColor green $_.Name, $_.VirtualMemorySize}}
+# 8.	Подсчитать размер занимаемый файлами в папке C:\windows (и во всех подпапках) за исключением файлов *.tmp
+"{0:N2} Gb" -f ((Get-ChildItem –force c:\Windows –Recurse -ErrorAction SilentlyContinue -Exclude *.tmp | Measure-Object Length -Sum).sum / 1Gb) 
+# 9.	Сохранить в CSV-файле информацию о записях одной ветви реестра HKLM:\SOFTWARE\Microsoft.
+Get-ChildItem HKLM:\SOFTWARE\Microsoft\ | Export-Csv C:\temp\Folder1\hklm.csv
+# 10.	Сохранить в XML -файле историческую информацию о командах выполнявшихся в текущем сеансе работы PS.
+Get-History | Export-Clixml C:\temp\Folder1\history.xml
+# 11.	Загрузить данные из полученного в п.10 xml-файла и вывести в виде списка информацию о каждой записи, в виде 5 любых (выбранных Вами) свойств.
+Import-Clixml C:\temp\Folder1\history.xml | Select-Object -Property id, commandline, ExecutionStatus, StartExecutionTime, EndExecutionTime
+# 12.	Удалить созданный диск и папку С:\M2T2_ФАМИЛИЯ
+Remove-PSDrive -Name Mypsdrive -PSProvider "FileSystem" -Force
+Remove-Item -Path C:\temp\Folder\ -Force
