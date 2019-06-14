6c3e138267f4e5cfd37bc0f3ebdd6553549ae6ed
pavlovskyantony <pavlovsky.antony@gmail.com>
2019-06-14 08:26:23 +0300

    new tasks

---
+++ Module_2/Task_5/ST_DevOps2_2019_PavlovskyAN_Module2_Task5.ps1
@@ -0,0 +1,23 @@
+﻿<# В задании используются виртуальные машины созданные в предыдущих модулях. #>
+
+# 1.	При помощи WMI перезагрузить все виртуальные машины.
+Get-WmiObject -Class Win32_operatingsystem -Credential $cred -ComputerName 10.2.2.1,10.2.2.3 | Invoke-WmiMethod -Name Reboot
+# 2.	При помощи WMI просмотреть список запущенных служб на удаленном компьютере. 
+$cred = Get-Credential home\Administrator
+Get-WmiObject win32_service -ComputerName 10.2.2.1 -Credential $cred |
+ Where-Object {$_.state -eq 'Running'} | Format-Table -Property Name, State, Status
+# 3.	Настроить PowerShell Remoting, для управления всеми виртуальными машинами с хостовой.
+Enable-PSRemoting
+# 4.	Для одной из виртуальных машин установить для прослушивания порт 42658. Проверить работоспособность PS Remoting.
+Set-Item WSMan:\localhost\Service\EnableCompatibilityHttpListener $true
+Set-Item WSMan:\localhost\Service\EnableCompatibilityHttpsListener $true
+Set-Item WSMan:\localhost\listener\listener*\port -Value 42658
+Enter-PSSession 10.2.2.1 -Port 42658
+# 5	Создать конфигурацию сессии с целью ограничения использования всех команд, кроме просмотра содержимого дисков.
+New-PSSessionConfigurationFile -Path c:\Temp\TestSessionConfiguration.pssc -VisibleCmdlets Get-ChildItem,Exit-PSSession
+# ТЕстируем файл конфигурации
+Test-PSSessionConfigurationFile c:\Temp\TestSessionConfiguration.pssc
+# Регистрируем файл конфигурации
+Register-PSSessionConfiguration -Name TestSessionConfiguration -Path c:\Temp\TestSessionConfiguration.pssc -RunAsCredential
+# Проверяем подключение, с помощью файла конфигурации
+New-PSSession -ComputerName 10.2.2.1 -ConfigurationName TestSessionConfiguration 
---
+++ Module_2/Task_6/ST_DevOps2_2019_PavlovskyAN_Module2_Task6.ps1
@@ -0,0 +1,70 @@
+# 1.	Для каждого пункта написать и выполнить соответсвующий скрипт автоматизации администрирования:
+# 1.1.	Вывести все IP адреса вашего компьютера (всех сетевых интерфейсов)
+Get-WmiObject -Class win32_networkadapterconfiguration -filter IPenabled=True | 
+Select-Object -Property description, ipaddress, ipsubnet
+# 1.2.	Получить mac-адреса всех сетевых устройств вашего компьютера.
+Get-WmiObject -Class win32_networkadapterconfiguration -filter IPenabled=True | 
+Select-Object -Property description, macaddress
+# 1.3.	На всех виртуальных компьютерах настроить (удалённо) получение адресов с DHСP.
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration -filter IPEnabled=true -ComputerName 10.2.2.1 |
+ ForEach-Object -Process {$_.InvokeMethod("EnableDHCP", $null)}
+# 1.4.	Расшарить папку на компьютере
+New-SmbShare -Name 'SharedFolder' -path 'c:\Temp\SharedFolder' -ContinuouslyAvailable -FullAccess 'home\Administrator' -ReadAccess 'home\GuestUsers'
+# 1.5.	Удалить шару из п.1.4
+Remove-SmbShare -Name SharedFolder -Force
+# 1.6.	Скрипт входными параметрами которого являются Маска подсети и два ip-адреса. Результат  – сообщение (ответ) в одной ли подсети эти адреса.
+#
+# 2.	Работа с Hyper-V
+# 2.1.	Получить список коммандлетов работы с Hyper-V (Module Hyper-V)
+Get-Command -Module Hyper-V
+# 2.2.	Получить список виртуальных машин 
+Get-VM | Format-List
+<#
+Name          State   CPUUsage(%) MemoryAssigned(M) Uptime           Status             Version
+----          -----   ----------- ----------------- ------           ------             -------
+pavlovsky_vm1 Running 0           2048              01:08:27.8810000 Operating normally 8.0
+pavlovsky_vm2 Running 0           2048              01:08:08.1390000 Operating normally 8.0
+pavlovsky_vm4 Off     0           0                 00:00:00         Operating normally 8.0
+#>
+# 2.3.	Получить состояние имеющихся виртуальных машин
+Get-VM | Select-Object Name, State
+<#
+Name            State
+----            -----
+pavlovsky_vm1 Running
+pavlovsky_vm2 Running
+pavlovsky_vm4     Off
+#>
+# 2.4.	Выключить виртуальную машину
+Get-VM -Name pavlovsky_vm2 | Stop-VM 
+<#
+Get-VM -Name pavlovsky_vm2
+Name          State CPUUsage(%) MemoryAssigned(M) Uptime   Status             Version
+----          ----- ----------- ----------------- ------   ------             -------
+pavlovsky_vm2 Off   0           0                 00:00:00 Operating normally 8.0
+#>
+# 2.5.	Создать новую виртуальную машину
+New-vm -Name TestVM -Generation 2 -Path 'C:\Temp\Vms' -MemoryStartupBytes 2GB -NewVHDPath 'C:\Temp\VHD' -NewVHDSizeBytes 64GB -SwitchName 'InternalSwitch'
+# 2.6.	Создать динамический жесткий диск
+New-VHD -Path 'C:\Temp\VHD\test.vhdx' -SizeBytes 64GB -Dynamic
+<#
+ComputerName            : homePC
+Path                    : C:\Temp\VHD\test.vhdx
+VhdFormat               : VHDX
+VhdType                 : Dynamic
+FileSize                : 4194304
+Size                    : 68719476736
+MinimumSize             :
+LogicalSectorSize       : 512
+PhysicalSectorSize      : 4096
+BlockSize               : 33554432
+ParentPath              :
+DiskIdentifier          : 7514BCAC-A50C-4FEB-A4FD-19BD83BA590A
+FragmentationPercentage : 0
+Alignment               : 1
+Attached                : False
+DiskNumber              :
+Number                  :
+#>
+# 2.7.	Удалить созданную виртуальную машину
+Get-VM -Name TestVM | Remove-VM
